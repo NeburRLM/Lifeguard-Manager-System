3316a59ae7ed7e8bc4aa049e60a78a3bbfdb8f52
Ruben Lopez Martinez <ruben.lopezm@estudiants.urv.cat>
2025-02-10 00:27:16 +0100

    Migrations started and tests done of facility-schema

---
+++ backend/postgresql/schemas/work-schedule.js
@@ -0,0 +1,31 @@
+export const WorkScheduleSchema = new EntitySchema({
+    name: "work_schedule",
+    tableName: "work_schedules",
+    columns: {
+        id: {
+            type: "uuid",
+            primary: true,
+            generated: "uuid"
+        },
+        month: {
+            type: "int"  // 1-12
+        },
+        year: {
+            type: "int"
+        }
+    },
+    relations: {
+        employee: {
+            type: 'many-to-one',
+            target: 'employee',
+            joinColumn: true,
+            onDelete: 'CASCADE'
+        },
+        schedules: {
+            type: 'one-to-many',
+            target: 'schedule',
+            inverseSide: 'work_schedule',
+            cascade: true
+        }
+    }
+});
