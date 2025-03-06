acece42ee73c83eacb34adaf0a9fcb78741aa500
Ruben Lopez Martinez <ruben.lopezm@estudiants.urv.cat>
2025-03-06 21:38:57 +0100

    Payrolls button added on sidebar

--- frontend/src/Pages/EmployeeView.jsx
+++ frontend/src/Pages/EmployeeView.jsx
@@ -237,6 +237,7 @@ const EmployeeView = () => {
             <li><Link to="/dashboard">Dashboard</Link></li>
             <li><Link to="/employees">Manage Employees</Link></li>
             <li><Link to="/facilities">Manage Facilities</Link></li>
+            <li><Link to="/payrolls">Manage Payrolls</Link></li>
             <li><Link to="/profile">Profile</Link></li>
             <li>
               <button className="logout-btn" onClick={signOut}>
--- frontend/src/Pages/ManageFacilities.jsx
+++ frontend/src/Pages/ManageFacilities.jsx
@@ -107,6 +107,7 @@ const handleDelete = (id) => {
             <li><Link to="/dashboard">Dashboard</Link></li>
             <li><Link to="/employees">Manage Employees</Link></li>
             <li><Link to="/facilities">Manage Facilities</Link></li>
+            <li><Link to="/payrolls">Manage Payrolls</Link></li>
             <li><Link to="/profile">Profile</Link></li>
             <li><button className="logout-btn" onClick={signOut}><FaSignOutAlt /> Sign Out</button></li>
           </ul>
